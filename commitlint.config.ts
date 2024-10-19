bcf0bc7c84ef387442b181cf80fae9285dd038ad
ponjs <ponjs@qq.com>
2024-10-19 16:43:38 +0800

    chore: husky

---
+++ commitlint.config.ts
@@ -0,0 +1,16 @@
+import type { UserConfig } from '@commitlint/types'
+import { RuleConfigSeverity } from '@commitlint/types'
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [RuleConfigSeverity.Error, 'never'],
+    'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
+    'type-empty': [RuleConfigSeverity.Error, 'never'],
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'chore', 'test'],
+    ],
+  },
+} satisfies UserConfig
