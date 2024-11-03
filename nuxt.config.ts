2de55ab2d37efb590dc154e8b7bbdf18103064a8
ponjs <ponjs@qq.com>
2024-11-04 00:46:58 +0800

    feat: custom i18n routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
   eslint: {
     config: {
       typescript: true,
     },
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh'],
   },
 })
